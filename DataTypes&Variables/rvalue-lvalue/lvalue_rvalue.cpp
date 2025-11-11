6f505b4dc4154e82a9eecb8e33723bb45ae88490
Trang Trinh <thtrang612@gmail.com>
2025-11-11 15:17:32 +0700

    DataVar-04 | rvalue and lvalue

---
+++ DataTypes&Variables/rvalue-lvalue/lvalue_rvalue.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 10;    //a - lvalue; 10 - rvalue
+    int b = 5;     //b - lvalue; 5 - rvalue
+    int c = a + b;  //c - lvalue; a+b - rvalue
+}
+
